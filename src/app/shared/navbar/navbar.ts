31bbb9ce364cbdfd723fd91eb704a22d51d0e71d
pablotorres0808 <pablodcoronadot@gmail.com>
2026-03-03 11:09:06 -0600

    pruebas de erik

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,11 +1,11 @@
 import { Component, signal, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './navbar.html',
   styles: ``,
 })
@@ -13,13 +13,14 @@ export class Navbar {
   private fb = inject(FormBuilder);
   isModalOpen = signal(false);
 
+  // el form para la comida
   formfood: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    price: ['', [Validators.required, Validators.min(0)]],
-    category: ['', [Validators.required, Validators.pattern('[A-Za-zÀ-ÿ\\s]+')]],
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    category: ['', Validators.required],
     available: [true],
-    description: [''],
-    url_img: ['']
+    description: ['', Validators.required],
+    url_img: ['', Validators.required]
   });
 
   saveFood() {
@@ -29,10 +30,8 @@ export class Navbar {
     }
 
     const foodData = this.formfood.value;
-    // Simulate saving the food here
-    console.log('Food saved locally (mock with Reactive Form):', foodData);
+    console.log('comida guardada:', foodData);
 
-    // Reset and close the modal
     this.formfood.reset({ available: true });
     this.isModalOpen.set(false);
   }
